176a1711c8aabe2a26d5a3b1d2a9a6680197742e
maskikuy <kproject305@gmail.com>
2025-11-23 21:29:49 +0700

    melengkapi admin js

--- models/admin.js
+++ models/admin.js
@@ -13,4 +13,26 @@ module.exports = (sequelize, DataTypes) => {
       autoIncrement: true,
       allowNull: false
     },
-    
+    username: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true
+    },
+    password_hash: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    created_at: { // Sesuai dengan SQL
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    }
+  }, {
+    sequelize,
+    modelName: 'Admin',
+    tableName: 'admins',
+    timestamps: true,      // Izinkan Sequelize mengelola timestamps
+    updatedAt: false,      // Tapi kita tidak punya kolom 'updated_at'
+    createdAt: 'created_at' // Map 'createdAt' ke kolom 'created_at'
+  });
+  return Admin;
+};
